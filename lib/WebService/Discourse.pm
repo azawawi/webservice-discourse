30266c4a90cfa5647f0a806fe4acb6d6107ab857
Ahmad M. Zawawi <azawawi@seladigital.com>
2018-12-11 11:42:57 +0200

    :hammer: Perl tidy

--- lib/WebService/Discourse.pm
+++ lib/WebService/Discourse.pm
@@ -28,36 +28,30 @@ use WebService::Discourse::UserActions;
 use WebService::Discourse::Users;
 
 with 'WebService::Discourse::ApiKey',
-    'WebService::Discourse::Backups',
-    'WebService::Discourse::Badges',
-    'WebService::Discourse::Categories',
-    'WebService::Discourse::Dashboard',
-    'WebService::Discourse::Email',
-    'WebService::Discourse::Groups',
-    'WebService::Discourse::Invite',
-    'WebService::Discourse::Notifications',
-    'WebService::Discourse::Posts',
-    'WebService::Discourse::PrivateMessages',
-    'WebService::Discourse::Search',
-    'WebService::Discourse::SiteSettings',
-    'WebService::Discourse::SSO',
-    'WebService::Discourse::Tags',
-    'WebService::Discourse::Topics',
-    'WebService::Discourse::Uploads',
-    'WebService::Discourse::UserActions',
-    'WebService::Discourse::Users';
+  'WebService::Discourse::Backups',
+  'WebService::Discourse::Badges',
+  'WebService::Discourse::Categories',
+  'WebService::Discourse::Dashboard',
+  'WebService::Discourse::Email',
+  'WebService::Discourse::Groups',
+  'WebService::Discourse::Invite',
+  'WebService::Discourse::Notifications',
+  'WebService::Discourse::Posts',
+  'WebService::Discourse::PrivateMessages',
+  'WebService::Discourse::Search',
+  'WebService::Discourse::SiteSettings',
+  'WebService::Discourse::SSO',
+  'WebService::Discourse::Tags',
+  'WebService::Discourse::Topics',
+  'WebService::Discourse::Uploads',
+  'WebService::Discourse::UserActions',
+  'WebService::Discourse::Users';
 
-has 'host' => (
-    is => 'rw'
-);
+has 'host' => ( is => 'rw' );
 
-has 'api_key' => (
-    is => 'rw'
-);
+has 'api_key' => ( is => 'rw' );
 
-has 'api_username' => (
-    is => 'rw'
-);
+has 'api_username' => ( is => 'rw' );
 
 1;
 
